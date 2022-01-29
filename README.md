4b648562e0540e88893f9af532fe9ce903a44e46
JMJHOX <40309454+JMJHOX@users.noreply.github.com>
2022-01-29 18:31:06 -0500

    Feature pro future (#18)
    * Update README.md
    
    * Update README.md
    
    * adding documentation, separating funcions and removing smells
    
    * fixing sonarcloud
    
    * Update README.md
    
    * Update README.md
    
    * fixing unit tests
    
    * fixing unit tests
    
    * updating yml
    
    * typescriot and javascript library transformation
    
    * fixing unit testing
    
    * refactoring and correcting karma.conf.js
    
    * modifying readme.md

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # date-simplify
-Set of tools that simplifies calculations about dates and time  on your Angular App
+Set of tools that simplifies code about dates and time on your javascript & Typescript Applications!
 
 
 # Version
-This library is working with Angular 13
+## From Angular to javascript/Typescript
+This library was originally designed to be working for Angular 13, but now it has be re-design to be used by any project that has javascript or Typescrit.
 
 # Install
 You only have to install it using npm:
@@ -12,9 +13,9 @@ npm i @jmjhox/date-simplify
 ```
 
 ## How to use it
-To get Started, you have to import "DateSimplifyService" as a service within your Angular app:
+To get Started, you have to import "DateSimplify" as a service within your Angular app:
 ```
-import { DateSimplifyService } from '@jmjhox/date-simplify';
+import {DateSimplify} from '@jmjhox/date-simplify';
 ```
 
 In your app.module.ts
@@ -41,7 +42,7 @@ export class AppModule { }
 And finally, in your component where it will be used:
 ```
 import { Component, OnInit } from '@angular/core';
-import { DateSimplifyService } from 'projects/date-simplify/src/public-api';
+import { DateSimplify } from 'projects/date-simplify/src/public-api';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -83,6 +84,14 @@ Is a function that formats any date to yyyy/mm/dd , if for some reasons, the dat
 this.date_simply.dateFormat('12121998')
 ```
 It should Return 12/12/1998 on string format
+
+# dateFormatISO
+Is a function that formats any date to yyyy/mm/dd ISO FORMAT , if for some reasons, the date is impossible to get, it returns the same date introduced.
+## How to use it
+```
+this.date_simply.dateFormat('12101998')
+```
+It should Return 1998-10-12T00:00:00.000Z on string format
 
 # dateIsValid
 method to check if a date is valid or not. it only returns false or true.
---
+++ src/date-simplify/DateSimplifyService.ts
@@ -0,0 +1,121 @@
+import { format, isValid } from 'date-fns';
+import { DateLimit } from '../commons/constants';
+import { RegexEnum } from '../commons/regex.enum';
+
+
+
+export function getDateRangeLimitUTC(dateRange: number, ChangeDate?: string): string {
+    let DiffYear = 0;
+    let actualYear = 0;
+
+    if (ChangeDate) {
+        actualYear = new Date(ChangeDate).getUTCFullYear();
+    }
+    if (!ChangeDate) {
+        actualYear = new Date().getUTCFullYear();
+    }
+
+    if (dateRange) {
+        if (dateRange <= 0 || !dateRange) {
+            return 'Invalid Age';
+        }
+        const firstDayOfYear = new Date(actualYear, 0, 1);
+        DiffYear = Number(firstDayOfYear.getUTCFullYear() - Number(dateRange));
+        return format(new Date(DiffYear, 0, 1), 'yyyy');
+    }
+    return "Invalid Range";
+}
+
+export function getDateRangeLimitLocal(dateRange: number, ChangeDate?: string): string {
+    let DiffYear = 0;
+    let actualYear = 0;
+    if (ChangeDate) {
+        actualYear = new Date(ChangeDate).getFullYear();
+    }
+    if (!ChangeDate) {
+        actualYear = new Date().getFullYear();
+    }
+
+    if (dateRange) {
+        if (dateRange <= 0) {
+            return 'Invalid Age';
+        }
+        const firstDayOfYear = new Date(actualYear, 0, 1);
+        DiffYear = Number(firstDayOfYear.getFullYear() - Number(dateRange));
+        return format(new Date(DiffYear, 0, 1), 'yyyy');
+    }
+    return "Invalid Range";
+}
+
+export function dateFormat(documentFormat: string): string | undefined {
+    if (documentFormat) {
+        if (documentFormat.match(RegexEnum.FORMAT_DATE)) {
+            documentFormat = documentFormat.replace(RegexEnum.FORMAT_DATE, "$1/$2/$3");
+
+        }
+        if (!isValid(new Date(documentFormat))) {
+            return documentFormat;
+        }
+
+        return format(new Date(documentFormat), 'yyyy/dd/MM');
+    }
+    return 'Error';
+}
+
+export function dateFormatISO(documentFormat: string): string | undefined {
+    if (documentFormat) {
+        if (documentFormat.match(RegexEnum.FORMAT_DATE)) {
+            documentFormat = documentFormat.replace(RegexEnum.FORMAT_DATE, "$1/$2/$3");
+
+        }
+        if (!isValid(new Date(documentFormat))) {
+            return documentFormat;
+        }
+
+        return new Date(documentFormat).toISOString();
+    }
+    return 'Error';
+}
+export function dateIsValid(day: string, month: string, year: string): boolean {
+    return isValid(new Date(`${day}/${month}/${year}`));
+}
+
+export function dateIsValidFix(day: string, month: string, year: string): string {
+    let dayParser = dateCheck(Number(day), 'day');
+    let monthParser = dateCheck(Number(month), 'month');
+    let yearParser = Number(year);
+    return `${dayParser}/${monthParser}/${yearParser}`;
+}
+
+function dateCheck(
+    dateNumber: number,
+    typeDate: string) {
+    let numberParser = dateNumber;
+
+    if (dateNumber < DateLimit.DATE_LIMIT_MIN) {
+        dateNumber = Math.abs(numberParser);
+        numberParser = Math.abs(numberParser);
+    }
+    if (typeDate == 'day' && dateNumber > DateLimit.DATE_DAY_MAX) {
+        for (let i = 0; i < (dateNumber - DateLimit.DATE_DAY_MAX); i++) {
+            numberParser--;
+        }
+        return numberParser;
+    }
+
+    if (typeDate == 'month' && dateNumber > DateLimit.DATE_MONTH_MAX) {
+        for (let i = 0; i < (dateNumber - DateLimit.DATE_MONTH_MAX); i++) {
+            numberParser = numberParser - 1;
+        }
+        return numberParser;
+    }
+
+    if (dateNumber == DateLimit.DATE_LIMIT_MIN) {
+        numberParser = numberParser + 1;
+        return numberParser;
+    }
+    return numberParser;
+
+}
+
+
