956b6b9db60c0241aded6e0c879d4450f12ab308
JMJHOX <40309454+JMJHOX@users.noreply.github.com>
2022-02-12 19:25:30 -0500

    Release 02/12/2022 (#29)
    * Update README.md
    
    * Update README.md
    
    * adding documentation, separating funcions and removing smells
    
    * fixing sonarcloud
    
    * Update README.md
    
    * Update README.md
    
    * fixing unit tests
    
    * fixing unit tests
    
    * updating yml
    
    * typescriot and javascript library transformation
    
    * fixing unit testing
    
    * refactoring and correcting karma.conf.js
    
    * modifying readme.md
    
    * Updating readme
    
    * updating readme
    
    * fixing typos
    
    * updating version
    
    * Create TODOLIST.md
    
    * Update TODOLIST.md
    
    * Update TODOLIST.md
    
    * fix: upgrade core-js from 3.6.5 to 3.20.2 (#22)
    
    Snyk has created this PR to upgrade core-js from 3.6.5 to 3.20.2.
    
    See this package in npm:
    https://www.npmjs.com/package/core-js
    
    See this project in Snyk:
    https://app.snyk.io/org/jmjhox/project/2435b85c-63c9-4fc8-a8ef-358af271e4f1?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * Update README.md
    
    * Checking TODO list (#23)
    
    
    * removing unused dependencies, fixing tsconfig.json for react sintax and fixing typos
    
    * adding .ds_store to gitignore
    
    * fixing typos on TODO list
    
    * removing checks
    
    * removing smells from Regex
    
    * Delete .DS_Store
    
    * adding npmignore for tests
    
    * Upgrading CICD
    
    * Feature pro master (#26)
    
    * Updating version (#20)
    
    * Update README.md
    
    * Update README.md
    
    * adding documentation, separating funcions and removing smells
    
    * fixing sonarcloud
    
    * Update README.md
    
    * Update README.md
    
    * fixing unit tests
    
    * fixing unit tests
    
    * updating yml
    
    * typescriot and javascript library transformation
    
    * fixing unit testing
    
    * refactoring and correcting karma.conf.js
    
    * modifying readme.md
    
    * Updating readme
    
    * updating readme
    
    * fixing typos
    
    * Updating version (#21)
    
    * Update README.md
    
    * Update README.md
    
    * adding documentation, separating funcions and removing smells
    
    * fixing sonarcloud
    
    * Update README.md
    
    * Update README.md
    
    * fixing unit tests
    
    * fixing unit tests
    
    * updating yml
    
    * typescriot and javascript library transformation
    
    * fixing unit testing
    
    * refactoring and correcting karma.conf.js
    
    * modifying readme.md
    
    * Updating readme
    
    * updating readme
    
    * fixing typos
    
    * updating version
    
    * Update README.md
    
    * Upgrading CICD
    
    * Fixing CICD
    
    * release dev
    
    * Feature development (#27)
    
    * developing new functions
    
    * removimg unn files
    
    * removing files
    
    * removing imports not being used
    
    * testing new function
    
    * deprecating getdateformatUnix
    
    * deprecating dateFormat
    
    * renaming getDateLimit to getAgeLimit to be more specific
    
    * renaming function getAgeLimit
    
    * adding documentation on new function replacement
    
    * upgrading dependencie core-js
    
    * adding new documentation about how to use dateFormat and detailing functions like getAgeOnLimitUTC and getAgeLimitOnLocal (#28)
    
    Co-authored-by: Snyk bot <snyk-bot@snyk.io>

--- src/commons/regex.enum.ts
+++ src/commons/regex.enum.ts
@@ -1,5 +1,12 @@
 export class RegexEnum {
-    public static FORMAT_DATE: RegExp = /(\d{2})(\d{2})(\d{4})/g;
-    public static FORMAT_DATE_WITH_VERTICAL_SLASH: RegExp = /(\d{1,4})-(\d{1,4})-(\d{1,4})/g;
-    public static FORMAT_DATE_WITH_DIAGONAL_SLASH: RegExp = /\d{1,4}-\d{1,4}-\d{1,4}/g;
-  }
+  public static FORMAT_DATE: RegExp = /(\d{2})(\d{2})(\d{4})/g;
+  public static FORMAT_DATE_WITH_VERTICAL_SLASH: RegExp = /(\d{1,4})-(\d{1,4})-(\d{1,4})/g;
+  public static FORMAT_DATE_WITH_DIAGONAL_SLASH: RegExp = /^(\d{1,4})\/(\d{1,4})\/(\d{1,4})$/;
+
+  public static FORMAT_DATE__SLASH: RegExp = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/
+}
+export class RegexSplitter {
+  public static FORMAT_SPLITTER_DATE: string = "/(.{2})(.{2})(.{4})/";
+  public static FORMAT_SPLITTER_VERTICAL_SLASH: string = "-"
+  public static FORMAT_SPLITTER_DIAGONAL_SLASH: string = "/"
+}
--- src/tests/date-simplific.spec.ts
+++ src/tests/date-simplific.spec.ts
@@ -6,59 +6,58 @@ describe('HelloComponent', () => {
 
 
    /*
-   * getDateRangeLimitUTC
+   * getAgeLimitUTC
    */
-
-  it('should return correct date for restrict users below 18 years old on 1991 on UTC', () => {
-    const serviceResult = service.getDateRangeLimitUTC(18, '2009');
+  test('should return correct date for restrict users below 18 years old on 1991 on UTC', () => {
+    const serviceResult = service.getAgeLimitOnUTC(18, '2009');
     expect(serviceResult).toEqual('1991');
   });
-  it('should return correct date for restrict users below 18 years old on any date on UTC', () => {
-    const serviceResult = service.getDateRangeLimitUTC(5);
+ test('should return correct date for restrict users below 18 years old on any date on UTC', () => {
+    const serviceResult = service.getAgeLimitOnUTC(5);
     const dateComp = String(new Date().getUTCFullYear() - Number(5));
     expect(serviceResult).toEqual(dateComp);
   });
 
-  it('should return error when invalid age is introduced on UTC', () => {
-    const serviceResult = service.getDateRangeLimitUTC(-1);
+  test('should return error when invalid age is introduced on UTC', () => {
+    const serviceResult = service.getAgeLimitOnUTC(-1);
     expect(serviceResult).toBe('Invalid Age');
   });
 
-  it('should return errorwhen invalid input is introduced on UTC', () => {
-    const serviceResult = service.getDateRangeLimitUTC(NaN);
+  test('should return errorwhen invalid input is introduced on UTC', () => {
+    const serviceResult = service.getAgeLimitOnUTC(NaN);
     expect(serviceResult).toBe('Invalid Range');
   });
 
  /*
-  * getDateRangeLimitLocal
+  * getAgeLimitLocal
  *
  * 
  */
 
 
-  it('should return correct date for restrict users below 18 years old on 1990 on Local', () => {
+ test('should return correct date for restrict users below 18 years old on 1990 on Local', () => {
     const limitAge = 18;
     const limitYear = '2009';
-    const serviceResult = service.getDateRangeLimitLocal(limitAge, limitYear);
+    const serviceResult = service.getAgeLimitOnLocal(limitAge, limitYear);
     var actualYear = new Date(limitYear).getFullYear();
     const firstDayOfYear = new Date(actualYear, 0, 1);
     var DiffYear = Number(firstDayOfYear.getFullYear() - Number(limitAge));
     var LocalYearObtained = format(new Date(DiffYear, 0, 1), 'yyyy');
     expect(serviceResult).toEqual(LocalYearObtained);
   });
-  it('should return correct date for restrict users below 18 years old on any date on Local', () => {
-    const serviceResult = service.getDateRangeLimitLocal(5);
+  test('should return correct date for restrict users below 18 years old on any date on Local', () => {
+    const serviceResult = service.getAgeLimitOnLocal(5);
     const dateComp = String(new Date().getFullYear() - Number(5));
     expect(serviceResult).toEqual(dateComp);
   });
 
-  it('should return error when invalid age is introduced on Local', () => {
-    const serviceResult = service.getDateRangeLimitLocal(-1);
+  test('should return error when invalid age is introduced on Local', () => {
+    const serviceResult = service.getAgeLimitOnLocal(-1);
     expect(serviceResult).toBe('Invalid Age');
   });
 
-  it('should return errorwhen invalid input is introduced on Local', () => {
-    const serviceResult = service.getDateRangeLimitLocal(NaN);
+  test('should return errorwhen invalid input is introduced on Local', () => {
+    const serviceResult = service.getAgeLimitOnLocal(NaN);
     expect(serviceResult).toBe('Invalid Range');
   });
 
@@ -67,102 +66,35 @@ describe('HelloComponent', () => {
    */
 
 
-
-  it('should return date format ISO 1', () => {
-    const serviceResult = service.dateFormatISO('12121998');
-    expect(serviceResult).toBe(new Date('1998-12-12').toISOString());
-  });
-
-  it('should return error when is void ISO', () => {
-    const serviceResult = service.dateFormatISO('');
-    expect(serviceResult).toBe('Error');
-  });
-
-  it('should return date format 2 ISO', () => {
-    const serviceResult = service.dateFormatISO('11/12/1998');
-    expect(serviceResult).toBe(new Date('1998-11-12').toISOString());
-  });
-
-  it('should return date format 3 ISO', () => {
-    const serviceResult = service.dateFormatISO('12.12.1998');
-    expect(serviceResult).toBe(new Date('1998-12-12').toISOString());
-  });
-  it('should return date format 4 ISO', () => {
-    const serviceResult = service.dateFormatISO('1998.10.12');
-    expect(serviceResult).toBe(new Date('1998-10-12').toISOString());
-  });
-
-  it('should return same date format when is invalid but formatted NOT IN ISO', () => {
-    const serviceResult = service.dateFormatISO('00121998');
-    expect(serviceResult).toBe('00/12/1998');
-  });
-
-
-
-  /*
-   * dateFormat
-   */
-
-
-  it('should return date format 1', () => {
-    const serviceResult = service.dateFormat('12121998');
-    expect(serviceResult).toBe('1998/12/12');
-  });
-
-  it('should return error when is void', () => {
-    const serviceResult = service.dateFormat('');
-    expect(serviceResult).toBe('Error');
-  });
-
-  it('should return date format 2', () => {
-    const serviceResult = service.dateFormat('11/12/1998');
-    expect(serviceResult).toBe('1998/12/11');
-  });
-
-  it('should return date format 3', () => {
-    const serviceResult = service.dateFormat('12.12.1998');
-    expect(serviceResult).toBe('1998/12/12');
-  });
-  it('should return date format 4', () => {
-    const serviceResult = service.dateFormat('1998.10.12');
-    expect(serviceResult).toBe('1998/12/10');
-  });
-
-  it('should return same date format when is invalid but formatted', () => {
-    const serviceResult = service.dateFormat('00121998');
-    expect(serviceResult).toBe('00/12/1998');
-  });
-
-
   /*
    * dateIsValid
    */
 
 
-  it('should  check if date is correct ', () => {
+  test('should  check if date is correct ', () => {
     const serviceResult = service.dateIsValid('12', '12', '1998');
     expect(serviceResult).toBe(true);
   });
 
-  it('should  check if date is false ', () => {
+  test('should  check if date is false ', () => {
     const serviceResult = service.dateIsValid('0', '12', '1998');
     expect(serviceResult).toBe(false);
   });
 
-  it('should  fix bad date if date is wrong 1', () => {
+  test('should  fix bad date if date is wrong 1', () => {
     const serviceResult = service.dateIsValidFix('0', '13', '1998');
     expect(serviceResult).toBe('1/12/1998');
   });
 
-  it('should  fix bad date if date is wrong 2 ', () => {
+  test('should  fix bad date if date is wrong 2 ', () => {
     const serviceResult = service.dateIsValidFix('32', '0', '1998');
     expect(serviceResult).toBe('31/1/1998');
   });
-  it('should  fix bad date if date is wrong 3 ', () => {
+  test('should  fix bad date if date is wrong 3 ', () => {
     const serviceResult = service.dateIsValidFix('-1', '0', '1998');
     expect(serviceResult).toBe('1/1/1998');
   });
-  it('should return bad date if a error has occured ', () => {
+  test('should return bad date if a error has occured ', () => {
     const serviceResult = service.dateIsValidFix('12', '1', '1');
     expect(serviceResult).toBe('12/1/1');
   });
@@ -172,11 +104,54 @@ describe('HelloComponent', () => {
    * dateFormatNew
    */
 
-    // it('should return date format 1', () => {
-    //   const serviceResult = service.dateFormatNew('1998/02/23','ddMMyyyy','')
-    //   expect(serviceResult).toBe('1998/12/12');
-    // });
+    test('should return date format new function date 1', () => {
+      const serviceResult = service.dateFormat('02/23/1998','ddMMyyyy')
+      expect(serviceResult).toBe('02231998');
+    });
 
+    test('should return date format new function date 2', () => {
+      const serviceResult = service.dateFormat('02-23-1998','ddMMyyyy')
+      expect(serviceResult).toBe('02231998');
+    });
+
+    test('should return date format new function date 3', () => {
+      const serviceResult = service.dateFormat('02231998','ddMMyyyy')
+      expect(serviceResult).toBe('02231998');
+    });
+
+    test('should return date format new function date 4', () => {
+      const serviceResult = service.dateFormat('02-23-1998','dd-MM-yyyy')
+      expect(serviceResult).toBe('02-23-1998');
+    });
     
+    test('should return date format new function date 5', () => {
+      const serviceResult = service.dateFormat('02-23-1998','dd/MM/yyyy')
+      expect(serviceResult).toBe('02/23/1998');
+    });
+
+    test('should return date format new function date 6', () => {
+      const serviceResult = service.dateFormat('02-23-1998','dd.MM.yyyy')
+      expect(serviceResult).toBe('02-23-1998');
+    });
+
+    test('should return date format new function date ISO 1', () => {
+      const serviceResult = service.dateFormat('02-23-1998','ISO')
+      expect(serviceResult).toBe(new Date('02-23-1998').toISOString());
+    });
+
+    test('should return date format new function date ISO 2', () => {
+      const serviceResult = service.dateFormat('02/23/1998','ISO')
+      expect(serviceResult).toBe(new Date('02/23/1998').toISOString());
+    });
+
+    test('should return date format new function date ISO 3', () => {
+      const serviceResult = service.dateFormat('02-23-1998','ISO')
+      expect(serviceResult).toBe(new Date('02/23/1998').toISOString());
+    });
+
+    test('should return date format new function date UNIX 1', () => {
+      const serviceResult = service.dateFormat('02-23-1998','UNIX')
+      expect(serviceResult).toBe(888192000000);
+    });
 
 });
